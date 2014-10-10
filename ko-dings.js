216f7ea34452e426cdf9a876c8727931fa26673b
Christophe Desguez <christophe.desguez@gmail.com>
2014-10-10 17:17:03 +0200

    ADDED: Wrap of the default Knockout Binding Provider so that binding error are now signaled with a nice error class and an hover title. FIXED: deal with simple properties in className and toggleClass bindings

--- ko-dings.js
+++ ko-dings.js
@@ -11,6 +11,45 @@
 		return;
 	}
 
+	/**
+	 * Extract a value from a property accessor that may be an observable or a simple property
+	 */
+	function decodeValue(accessor) {
+		if (typeof accessor !== "function") return undefined;
+
+		var firstVal = accessor();
+		return (typeof firstVal === "function") ? firstVal() : firstVal;
+	}
+
+	// =============================================================
+	// Wrap the default Knockout Binding Provider so that
+	// binding errors don't throw exceptions anymore
+	// =============================================================
+	var koBindingProvider = ko.bindingProvider.instance;
+
+	ko.bindingProvider.instance = {
+		nodeHasBindings: koBindingProvider.nodeHasBindings,
+		getBindings: function(node, bindingContext) {
+			var bindings;
+			try {
+				bindings = koBindingProvider.getBindings(node, bindingContext);
+
+			} catch (ex) {
+				console.log("binding error", ex.message, node, bindingContext);
+				$(node).data("error", {
+					error: ex.message,
+					context: bindingContext
+				}).attr("title", ex.message).addClass("error");
+			}
+
+			return bindings;
+		}
+	};
+
+	$(window).on("hover", ".error", function(evt) {
+		$(this).css("border", "2px solid red");
+	});
+
 	// =============================================================
 	// Add new custom mappings for usual attributes href, src, etc..
 	// =============================================================
@@ -36,7 +75,7 @@
 	// =============================================================
 	bindingHandlers["className"] = {
 		init: function (element) {
-			var dataBinds = $(element).data("bind").split(";");
+			var dataBinds = $(element).data("bind").split(",");
 
 			dataBinds.forEach(function(dataBind) {
 				var definitionParts = dataBind.split(":"),
@@ -55,9 +94,11 @@
 			});
 		},
 		update: function (element, valueAccessor) {
-			var newClass = valueAccessor()(),
+			var newClass = decodeValue(valueAccessor),
 				eltClassName = element.className;
-			if (!eltClassName) {
+			if (!newClass) {
+				return;
+			} else if (!eltClassName) {
 				element.className = newClass;
 			} else {
 				element.className = eltClassName.replace($(element).data(newClass + "-update"), "")
@@ -67,6 +108,47 @@
 		preprocess: function(value, name, addBinding) {
 			var bindingParts = value.match(/([\w-]+)/gi), // extract words in pattern attribute[class1|class2]
 				accessorName = bindingParts.shift();
+
+			return accessorName;
+		}
+	};
+
+	/**
+	 * Usage 1 :
+	 * Toggle the class 'loading' according to the boolean value of the accessor of the same name
+	 * <div data-bind="toggleClass: loading"></div>
+
+	 * Usage 2 :
+	 * Toggle the class 'loading' according to the boolean value of the accessor 'isLoading'
+	 * <div data-bind="toggleClass: loading[isLoading]"></div>
+	 */
+	bindingHandlers["toggleClass"] = {
+		init: function (element) {
+			var dataBinds = $(element).data("bind").split(",");
+
+			dataBinds.forEach(function(dataBind) {
+				var definitionParts = dataBind.split(":"),
+					key = definitionParts[0].trim(),
+					bindExpr = definitionParts[1].trim();
+
+				if (key === "toggleClass") {
+					var bindingParts = bindExpr.match(/([\w-]+)/gi), // extract words in pattern className[accessor]
+						accessorName = bindingParts.pop(),
+						className = bindingParts.length ? bindingParts.pop() : accessorName;
+
+					$(element).data("toggleClass", className);
+				}
+			});
+		},
+		update: function (element, valueAccessor) {
+			var hasClass = decodeValue(valueAccessor),
+				className = $(element).data("toggleClass");
+
+			$(element).toggleClass(className, hasClass);
+		},
+		preprocess: function(value, name, addBinding) {
+			var bindingParts = value.match(/([\w-]+)/gi), // extract words in pattern attribute[class1|class2]
+				accessorName = bindingParts.pop();
 
 			return accessorName;
 		}
